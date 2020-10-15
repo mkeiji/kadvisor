7397363962fc0f427519b0007b394b72a2015845
Keiji <keiji@kgpc-wsl2>
2020-10-15 17:19:08 -0500

    OP-0 hide test store button

--- client/apps/kadvisor-app/src/app/react-placeholder/react-placeholder.tsx
+++ client/apps/kadvisor-app/src/app/react-placeholder/react-placeholder.tsx
@@ -5,6 +5,19 @@ import logo from '../../assets/logo.svg';
 import { Button } from 'react-bootstrap';
 
 class ReactPlaceholder extends Component<ReactPlaceholderPropTypes> {
+    showTestStoreButton = false;
+
+    getTestStoreButton = () => {
+        return (
+            <Button
+                variant={'outline-danger'}
+                onClick={() => this.props.testStoreFunc('HELLO TEST')}
+            >
+                Add Store with Redux
+            </Button>
+        );
+    };
+
     render() {
         return (
             <div className="Main-header">
@@ -12,20 +25,7 @@ class ReactPlaceholder extends Component<ReactPlaceholderPropTypes> {
                 <p>
                     This is <code>kadvisor</code> home-page.
                 </p>
-                <a
-                    className="Main-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Built on React
-                </a>
-                <Button
-                    variant={'outline-danger'}
-                    onClick={() => this.props.testStoreFunc('HELLO TEST')}
-                >
-                    Add Store with Redux
-                </Button>
+                {this.showTestStoreButton ? this.getTestStoreButton : null}
             </div>
         );
     }
